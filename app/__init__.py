37e55ebf9b79489f8a430a2ef397ff04d281ebea
braianleonardocano@gmail.com <braianleonardocano@gmail.com>
2022-09-22 10:34:48 -0300

    error page

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 import os
@@ -13,21 +13,30 @@ db = SQLAlchemy(app)
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(30),)
-    email = db.Column(db.String(50),)
-    password = db.Column(db.String(100),)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/singup", methods=['POST'])
+@app.route("/singup", methods=['GET','POST'])
 def singup():
-    return "Ingreso en singup"
+    if request.method == 'POST':
+        print(request.form)
+        return "Ingreso en singup"
+    else:
+        return redirect("error")
 
 @app.route("/login", methods=['GET'])
 def login():
     return "Ingreso en login"
+
+@app.errorhandler(404)
+def error(e):
+    return render_template("error.html"),404
+
 
 if __name__ == '__main__':
     db.create_all()
